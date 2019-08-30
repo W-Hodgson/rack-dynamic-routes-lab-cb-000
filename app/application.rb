5605d95ad45621b57f66115f6bbba5e64ace4e32
Will Hodgson <will.hodgson@current-rms.com>
2019-08-30 09:05:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items").last
       item = @@items.find{|item| item.name = item_name}
+      item.price
     else
       resp.write "Route not found"
       resp.status = 404
